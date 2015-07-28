3446469042d81ddc786ea5cbd82bda3d90e0b41d
mtztkt <mtztkt@gmail.com>
2015-07-28 17:59:54 +0300

    a
    a

---
+++ OmerHesapMakinesi/OmerHesapMakinesi/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OmerHesapMakinesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox2.Text += "1"; // C# - Java - C++ - ASP.Net -JSF - MVC -Javascript - Jquery- Ajax
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox2.Text += "2";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox2.Text += "3";
+        }
+    }
+}
